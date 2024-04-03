921ba65b73cd6a7388442fe729ed57747f927ca1
mostofa-s-cse <mostofa.s.cse@gmail.com>
2024-04-03 12:52:34 +0600

    completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,22 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Upload</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     @if (session('success'))
-        <div>{{ session('success') }}</div>
+        <!-- Alert Popup Modal -->
+        <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="successModalLabel">Success!</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        {{ session('success') }}
+                    </div>
+                </div>
+            </div>
+        </div>
     @endif
 
-    <form action="{{ route('image.upload') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <input type="file" name="images[]" multiple accept="image/*">
-        <button type="submit">Upload</button>
-    </form>
+    <div class="container mt-5">
+        <form action="{{ route('image.upload') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3">
+                <input type="file" class="form-control" name="images[]" multiple accept="image/*" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Upload</button>
+        </form>
 
-    <a href="{{ route('image.download') }}">Download All Images as Zip</a>
-    {{-- <a href="{{ route('image.download', ['id' => 1]) }}">Download All Images as Zip</a> --}}
-    <br/>
-    <a href="{{ route('image.singleDownload', ['id' => 12]) }}">singleDownload Images as Zip</a>
+        <a href="{{ route('image.download') }}" class="btn btn-success mt-3">Download All Images as Zip</a>
+        <br/>
+        <a href="{{ route('image.singleDownload', ['id' => 12]) }}" class="btn btn-primary mt-3">singleDownload Images as Zip</a>
+    </div>
 
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script>
+        // Show the success modal if success message exists
+        @if (session('success'))
+            $(document).ready(function(){
+                $('#successModal').modal('show');
+            });
+        @endif
+    </script>
 </body>
 </html>
